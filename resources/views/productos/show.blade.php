9b3ed2bb3ffe4c8c4702ab162d98121b60d50271
Estefany Natali Velasquez Yox <natalivelasquez@MacBook-Air-de-Estefany.local>
2025-11-20 18:00:53 -0600

    fix: corregir visualización del nombre de la categoría de productos

--- resources/views/productos/show.blade.php
+++ resources/views/productos/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Detalle Producto - Proserve S.A.') }}
+            {{ __('Detalle Producto - Proserve') }}
         </h2>
     </x-slot>
 
@@ -9,6 +9,8 @@
         <div class="bg-white p-6 rounded shadow">
             <h3 class="text-xl font-semibold mb-4">{{ $producto->descripcion }}</h3>
             <p><strong>Código:</strong> {{ $producto->codigo }}</p>
+            <p><strong>Descripción:</strong> {{ $producto->descripcion }}</p>
+            <p><strong>Categoría:</strong> {{ $producto->categoria->nombre }}</p>
             <p><strong>Precio Unitario:</strong> Q{{ number_format($producto->precio_unitario, 2) }}</p>
             <p><strong>Stock Actual:</strong> {{ $producto->stock_actual }}</p>
         </div>
