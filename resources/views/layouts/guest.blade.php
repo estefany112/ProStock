88f39d7902fc9180bb68696a697e35d6e8e66ace
Estefany Natali Velasquez Yox <natalivelasquez@MacBook-Air-de-Estefany.local>
2025-11-27 23:01:22 -0600

    redirección al welcome

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -6,19 +6,39 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'ProStock') }}</title>
 
-    <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body class="relative min-h-screen font-sans antialiased" onclick="location.href='{{ url('/') }}'">
-    <div class="absolute inset-0 bg-cover bg-center z-0" style="background-image: url('{{ asset('assets/img/icono.png') }}');"></div>
-    <div class="absolute inset-0 bg-gradient-to-b from-black/60 to-black/40 z-0"></div>
+<body class="relative min-h-screen font-sans antialiased">
 
-    <div class="relative z-10 flex items-center justify-center min-h-screen px-4">
-        {{ $slot }}
+    <!-- FONDO CLICKEABLE -->
+    <div 
+        class="absolute inset-0 z-0 cursor-pointer pointer-events-auto"
+        onclick="location.href='{{ url('/') }}'">
+
+        <img 
+            src="{{ asset('assets/img/icono.png') }}" 
+            class="w-full h-full object-cover pointer-events-none"
+        >
     </div>
+
+    <!-- GRADIENTE -->
+    <div 
+        class="absolute inset-0 bg-gradient-to-b from-black/60 to-black/40 z-0 pointer-events-none">
+    </div>
+
+    <!-- CONTENEDOR DEL FORMULARIO (NO BLOQUEA EL FONDO) -->
+    <div class="relative z-10 min-h-screen flex items-center justify-center px-4 pointer-events-none">
+
+        <!-- FORMULARIO CENTRADO Y CLICKEABLE -->
+        <div class="pointer-events-auto w-full max-w-md">
+            {{ $slot }}
+        </div>
+
+    </div>
+
 </body>
 
 </html>
